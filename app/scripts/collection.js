941800de82d978f913ce889450e7f722062debe0
Jeremy Crawford <jcrawford.design@gmail.com>
2014-02-26 14:14:24 -0500

    Made changes to error

--- app/scripts/collection.js
+++ app/scripts/collection.js
@@ -2,10 +2,10 @@ var ParkPics = Backbone.Model.extend({});
 
 var ParkPicsCollection = Backbone.Collection.extend({
 	model: ParkPics,
+})
 
   // url: // This is where your url goes from the api that your fetching
 
   // parse: function(response) {
   //   return response.results;
   // }
-})
